d0813dc8c6083ad9402bceb437d12f891846e443
손동민 <sondmmm0308@gmail.com>
2025-08-01 18:47:32 +0900

    [feat] 로그인 및 회원가입 API - #4

--- board/src/main/java/server/board/domain/home/controller/HomeController.java
+++ board/src/main/java/server/board/domain/home/controller/HomeController.java
@@ -1,11 +1,17 @@
 package server.board.domain.home.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import server.board.domain.home.dto.LoginRequestDto;
 import server.board.domain.home.service.HomeService;
+import server.board.domain.user.dto.UserCreateRequestDto;
+import server.board.global.jwt.JwtToken;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,13 +22,14 @@ public class HomeController {
 
     // 로그인(/api/sign-in)
     @PostMapping("/sign-in")
-    public ResponseEntity<?> signIn(){
-
+    public ResponseEntity<JwtToken> signIn(@Valid @RequestBody LoginRequestDto loginRequestDto) {
+        return ResponseEntity.status(HttpStatus.OK).body(homeService.signIn(loginRequestDto));
     }
 
     // 회원가입(/api/sign-up)
     @PostMapping("/sign-up")
-    public ResponseEntity<?>  signUp(){
-
+    public ResponseEntity<?>  signUp(@Valid @RequestBody UserCreateRequestDto userCreateRequestDto){
+        homeService.signUp(userCreateRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
--- board/src/main/java/server/board/domain/home/service/HomeService.java
+++ board/src/main/java/server/board/domain/home/service/HomeService.java
@@ -1,13 +1,41 @@
 package server.board.domain.home.service;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import server.board.domain.home.dto.LoginRequestDto;
+import server.board.domain.user.dto.UserCreateRequestDto;
+import server.board.domain.user.entity.User;
 import server.board.domain.user.repository.UserRepository;
+import server.board.global.jwt.JwtToken;
+import server.board.global.jwt.JwtTokenProvider;
 
 @Service
 @RequiredArgsConstructor
 public class HomeService {
 
     private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
+    @Transactional(readOnly = true)
+    public JwtToken signIn(@Valid LoginRequestDto loginRequestDto) {
+        UsernamePasswordAuthenticationToken authenticationToken
+                = new UsernamePasswordAuthenticationToken(loginRequestDto.getEmail(), loginRequestDto.getPassword());
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        return jwtTokenProvider.generateToken(authentication);
+    }
+
+    public void signUp(@Valid UserCreateRequestDto userCreateRequestDto) {
+        userRepository.save(User.create(
+                userCreateRequestDto,
+                bCryptPasswordEncoder.encode(userCreateRequestDto.getPassword())
+        ));
+    }
 }
