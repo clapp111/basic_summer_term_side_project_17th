79e7eaa2309e7198a804255c2fd99e64ced0dcb9
손동민 <sondmmm0308@gmail.com>
2025-08-01 12:29:20 +0900

    [feat] 로그인 기능 및 기본 예외처리기 세팅 - #4

--- board/src/main/java/server/board/domain/user/repository/UserRepository.java
+++ board/src/main/java/server/board/domain/user/repository/UserRepository.java
@@ -3,5 +3,10 @@ package server.board.domain.user.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import server.board.domain.user.entity.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByEmail(String email);
+
 }
---
+++ board/src/main/java/server/board/global/config/SecurityConfig.java
@@ -0,0 +1,56 @@
+package server.board.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import server.board.global.jwt.JwtAuthenticationFilter;
+import server.board.global.jwt.JwtExceptionFilter;
+import server.board.global.jwt.JwtTokenProvider;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+
+public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/sign-up", "/sign-in").permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+
+                .addFilterBefore(new JwtExceptionFilter(), JwtAuthenticationFilter.class)
+
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
