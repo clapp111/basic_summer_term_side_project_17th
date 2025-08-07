79e6667d3eec73bbc8d66950d38e6becb8dbfaa9
손동민 <sondmmm0308@gmail.com>
2025-08-07 14:48:52 +0900

    [docs] 유저, 로그인 및 회원가입 swagger 명세서 작성

--- board/src/main/java/server/board/global/exception/error/CustomErrorCode.java
+++ board/src/main/java/server/board/global/exception/error/CustomErrorCode.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum CustomErrorCode implements ErrorCode {
 
     //400 Bad Request
-    INVALID_REQUEST(HttpStatus.BAD_REQUEST, 400, "인자값이 유효하지 않음"),
+    INVALID_REQUEST(HttpStatus.BAD_REQUEST, 400, "유효하지 않은 인자값입니다"),
     INVALID_PART_VALUE(HttpStatus.BAD_REQUEST, 400, "유효하지 않은 파트입니다."),
     INVALID_PASSWORD_VALUE(HttpStatus.BAD_REQUEST, 400, "일치하지 않은 패스워드입니다."),
 
@@ -18,9 +18,9 @@ public enum CustomErrorCode implements ErrorCode {
     ASSIGNMENT_DELETE_FORBIDDEN(HttpStatus.FORBIDDEN, 403, "로그인한 유저의 해당 과제 삭제 권한이 없음"),
 
     // 404 NOT_FOUND
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND, 404,"존재하지 않는 회원"),
-    ASSIGNMENT_NOT_FOUND(HttpStatus.NOT_FOUND, 404,"존재하지 않는 과제"),
-    RECOMMENDATION_NOT_FOUND(HttpStatus.NOT_FOUND, 404,"추천 기록이 없습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, 404,"유저가 존재하지 않습니다"),
+    ASSIGNMENT_NOT_FOUND(HttpStatus.NOT_FOUND, 404,"과제가 존재하지 않습니다"),
+    RECOMMENDATION_NOT_FOUND(HttpStatus.NOT_FOUND, 404,"추천 기록이 존재하지 않습니다."),
 
     // 409 CONFLICT
     DUPLICATE_USER(HttpStatus.CONFLICT, 409, "이미 가입된 이메일입니다."),
